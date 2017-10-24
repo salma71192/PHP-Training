eb2660f870938601b6fc2ea7728b6577d328335c
Salma Mohammed <sm.71175@gmail.com>
2017-10-24 21:21:03 +0200

    Include Variables in HTML

---
+++ variables.php
@@ -0,0 +1,19 @@
+<?php 
+
+// Include Variables in different Parts in HTML Document
+
+$variable = "PHP Tutorial";  ?>
+
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+   "http://www.w3.org/TR/html4/loose.dtd">
+
+<html lang="en">
+	<head>
+		<title><?php echo $variable; ?></title>
+	</head>
+	<body>
+            <h3><?php echo $variable; ?></h3>
+            <a href="#"><?php echo $variable; ?></a>
+	</body>
+</html>
